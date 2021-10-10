f31270f0e0ae38f871fccc897270bb3c9e41f483
Cc_17 <Cc_17yyxovo@163.com>
2021-10-10 19:34:07 +0800

    Completed.

--- src/LinkedList/OrderedMerge.java
+++ src/LinkedList/OrderedMerge.java
@@ -11,7 +11,8 @@ public class OrderedMerge {
 		LinkedListNode firstNode = mock(firstValues, firstValues.size() - 1, null);
 		List<Integer> secondValues = Arrays.asList(300, 700, 1800, 2100);
 		LinkedListNode secondNode = mock(secondValues, secondValues.size() - 1, null);
-		System.out.println(merge(firstNode, secondNode));
+		//System.out.println(mergeByRebuild(firstNode, secondNode));
+		System.out.println(mergeBySimple(firstNode, secondNode));
 	}
 
 	private static LinkedListNode mock(List<Integer> values, int index, LinkedListNode node) {
@@ -27,7 +28,7 @@ public class OrderedMerge {
 		return mock(values, index - 1, newNode);
 	}
 
-	private static LinkedListNode merge(LinkedListNode firstNode, LinkedListNode secondNode) {
+	private static LinkedListNode mergeByRebuild(LinkedListNode firstNode, LinkedListNode secondNode) {
 		List<Integer> values = new ArrayList<>();
 		while (firstNode != null) {
 			values.add(firstNode.getValue());
@@ -40,19 +41,19 @@ public class OrderedMerge {
 		Collections.sort(values);
 		return mock(values, values.size() - 1, null);
 	}
+
+	private static LinkedListNode mergeBySimple(LinkedListNode firstNode, LinkedListNode secondNode){
+		return null;
+	}
 }
 
 class LinkedListNode {
-	private int value;
+	private final int value;
 	private LinkedListNode nextNode;
 
 	public LinkedListNode(int value, LinkedListNode nextNode) {
 		this.value = value;
 		this.nextNode = nextNode;
-	}
-
-	public void setValue(int value) {
-		this.value = value;
 	}
 
 	public void setNextNode(LinkedListNode nextNode) {
